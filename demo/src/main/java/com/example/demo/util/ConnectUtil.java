c95ffd6b9ef22e2c71e56bee5ae9fb88c3bccb59
jun <a1125506303@gmail.com>
2019-01-08 10:21:29 +0800

    no message

--- demo/src/main/java/com/example/demo/util/ConnectUtil.java
+++ demo/src/main/java/com/example/demo/util/ConnectUtil.java
@@ -1,10 +1,7 @@
 package com.example.demo.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.Resource;
 import java.sql.Connection;
 import java.sql.DriverManager;
