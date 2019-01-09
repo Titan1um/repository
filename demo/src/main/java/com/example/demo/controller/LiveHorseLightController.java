735b573942a14902e028efbddf24c5a0a5d5c2f2
jun <a1125506303@gmail.com>
2019-01-09 15:18:26 +0800

    -修改跑马灯:加入@CrossOrigin 在bin下加入crossdomain

--- demo/src/main/java/com/example/demo/controller/LiveHorseLightController.java
+++ demo/src/main/java/com/example/demo/controller/LiveHorseLightController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.LiveHorseLight;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@CrossOrigin
 public class LiveHorseLightController {
 	@Autowired
 	LiveHorseLight liveHorseLight;
