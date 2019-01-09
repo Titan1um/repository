b22141f1cfd19a11760fc2231aa053bdded36824
jun <a1125506303@gmail.com>
2019-01-09 18:50:34 +0800

    -测试urlEncode

--- demo/src/main/java/com/example/demo/service/LiveValidator.java
+++ demo/src/main/java/com/example/demo/service/LiveValidator.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
 import javax.servlet.ServletRequest;
@@ -11,13 +14,14 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import sun.misc.BASE64Encoder;
 
 /**
-* @Description:  两种验证方式secretkey不同，故有两个init版本
+ * @Description: 两种验证方式secretkey不同，故有两个init版本
  * 在第103行填入自定义授权secrekey
  * 在第127行填入外部授权secretkey,或搜索
-* @Author: LJH
-*/
+ * @Author: LJH
+ */
 @Component
 public class LiveValidator {
 	private String ts = null;
@@ -32,28 +36,45 @@ public class LiveValidator {
 	private String avatar = "http://live.polyv.net/assets/images/avatars/9avatar.jpg";
 	private int status = 1;
 
+
 	/**
-	* @Description:  自定义授权
+	 * @Description: just for test
+	 * @Author: LJH
+	 */ {
+		String tmp = nickname;
+		// 将普通字符创转换成application/x-www-from-urlencoded字符串
+		try {
+			BASE64Encoder encoder = new BASE64Encoder();
+			tmp = encoder.encode(tmp.getBytes(Charset.forName("UTF-8")));
+			nickname = URLEncoder.encode(tmp, "GBK");
+			System.out.println("========NickName:" + nickname + "========");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @Description: 自定义授权
 	 * 较多的system.out.println,用以测试查看输出结果,应删除
-	* @Param: [req]
-	* @return: java.lang.String
-	* @Author: LJH
-	*/
+	 * @Param: [req]
+	 * @return: java.lang.String
+	 * @Author: LJH
+	 */
 	public String getSDCallBack(HttpServletRequest req) {
 		//初始化参数
 		initSD(req);
 		System.out.println("========inside sd========");
-		System.out.println("sign:"+sign);
-		System.out.println("token:"+token);
+		System.out.println("sign:" + sign);
+		System.out.println("token:" + token);
 		//若接收到的sign 与 拼接计算后的token 相等则输出
 		if (sign.equals(token)) {
 			String tmp = secretkey + id + secretkey + ts + secretkey + userid;
 			sign = DigestUtils.md5Hex(tmp.getBytes(Charset.forName("UTF-8")));
 			String res = url + "?userid=" + userid + "&nickname=" + nickname + "&marqueeName=" + marqueeName
 					+ "&avatar=" + avatar + "&ts=" + ts + "&sign=" + sign;
-			System.out.println("res:"+res);
-			String finalRes = "<script language='javascript' type='text/javascript'>location.href='"+res+"'</script>";
-			System.out.println("finalRes:"+finalRes);
+			System.out.println("res:" + res);
+			String finalRes = "<script language='javascript' type='text/javascript'>location.href='" + res + "'</script>";
+			System.out.println("finalRes:" + finalRes);
 			return finalRes;
 		}
 		//错误则返回error(自定义)
@@ -62,18 +83,18 @@ public class LiveValidator {
 	}
 
 	/**
-	* @Description: 外部授权
-	* @Param: [req]
-	* @return: java.lang.String
-	* @Author: LJH
-	*/
+	 * @Description: 外部授权
+	 * @Param: [req]
+	 * @return: java.lang.String
+	 * @Author: LJH
+	 */
 	public String getOVCallBack(HttpServletRequest req) {
 		//初始化参数
 		initOV(req);
 		JSONObject json = new JSONObject();
 		System.out.println("========inside ov========");
-		System.out.println("sign:"+sign);
-		System.out.println("token:"+token);
+		System.out.println("sign:" + sign);
+		System.out.println("token:" + token);
 		//若接收到的token 与 拼接计算后的sign 不等则输出
 		if (!sign.equals(token)) {
 			json.put("status", 0);
@@ -92,104 +113,104 @@ public class LiveValidator {
 	}
 
 	/**
-	* @Description:  初始化外部授权的参数
-	* @Param: [req]
-	* @return: void
-	* @Author: LJH
-	*/
+	 * @Description: 初始化外部授权的参数
+	 * @Param: [req]
+	 * @return: void
+	 * @Author: LJH
+	 */
 	private void initOV(HttpServletRequest req) {
 		System.out.println("========inside initOV========");
 		//此处填入外部授权的secretKey
 		secretkey = "em31gCSY1h";
-		System.out.println("set secretkey:"+secretkey);
+		System.out.println("set secretkey:" + secretkey);
 		//获取req参数
 		token = req.getParameter("token");
-		System.out.println("set token:"+token);
+		System.out.println("set token:" + token);
 		ts = req.getParameter("ts");
-		System.out.println("set ts:"+ts);
+		System.out.println("set ts:" + ts);
 		userid = req.getParameter("userid");
-		System.out.println("set userid:"+userid);
+		System.out.println("set userid:" + userid);
 		//拼接计算sign
 		String plain = secretkey + userid + secretkey + ts;
-		System.out.println("set plain:"+plain);
+		System.out.println("set plain:" + plain);
 		sign = DigestUtils.md5Hex(plain.getBytes(Charset.forName("UTF-8")));
-		System.out.println("set sign:"+sign);
+		System.out.println("set sign:" + sign);
 	}
 
 	/**
-	* @Description:  初始化自定义授权的参数
-	* @Param: [req]
-	* @Author: LJH
-	*/
+	 * @Description: 初始化自定义授权的参数
+	 * @Param: [req]
+	 * @Author: LJH
+	 */
 	private void initSD(HttpServletRequest req) {
 		System.out.println("========inside initSD========");
 		//此处填入自定义授权的SecretKey
 		secretkey = "KWWXssNYBK";
-		System.out.println("set secretkey:"+secretkey);
+		System.out.println("set secretkey:" + secretkey);
 		//获取req参数
 		sign = req.getParameter("sign");
-		System.out.println("set sign:"+sign);
+		System.out.println("set sign:" + sign);
 		ts = req.getParameter("ts");
-		System.out.println("set ts:"+ts);
+		System.out.println("set ts:" + ts);
 		id = req.getParameter("id");
-		System.out.println("set id:"+id);
+		System.out.println("set id:" + id);
 		url = req.getParameter("url");
-		System.out.println("set url:"+url);
+		System.out.println("set url:" + url);
 		userid = "eciyhturt8";
-		System.out.println("set userid:"+userid);
+		System.out.println("set userid:" + userid);
 		//拼接计算token
 		String plain = secretkey + id + secretkey + ts;
-		System.out.println("set plain:"+plain);
+		System.out.println("set plain:" + plain);
 		token = DigestUtils.md5Hex(plain.getBytes(Charset.forName("UTF-8")));
-		System.out.println("set token:"+token);
+		System.out.println("set token:" + token);
 	}
 
 	/**
-	* @Description:  客户接入外部授权案例
-	* @Param: [request, response]
-	* @return: java.lang.String
-	* @Author: LJH
-	*/
+	 * @Description: 客户接入外部授权案例
+	 * @Param: [request, response]
+	 * @return: java.lang.String
+	 * @Author: LJH
+	 */
 	public String outerValidate(ServletRequest request, ServletResponse response) throws Exception {
-        try {
-        	secretkey = "em31gCSY1h";
+		try {
+			secretkey = "em31gCSY1h";
 
-        	//获得参数
-            String userid = request.getParameter("userid");
-            //模拟用户从数据库查询用户id
-            this.userid = request.getParameter("userid");
-            String token = request.getParameter("token");
-            System.out.println("token:"+token);
-            String ts = request.getParameter("ts");
+			//获得参数
+			String userid = request.getParameter("userid");
+			//模拟用户从数据库查询用户id
+			this.userid = request.getParameter("userid");
+			String token = request.getParameter("token");
+			System.out.println("token:" + token);
+			String ts = request.getParameter("ts");
 
-            //判断userid是否合法
-            if (userid != null && String.valueOf(userid).equals(this.userid)) {
-            	//计算sign
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.append(secretkey).append(userid).append(secretkey).append(ts);
-                String signSource = stringBuilder.toString();
-                System.out.println("plain:"+signSource);
-                String sign = DigestUtils.md5Hex(signSource.getBytes(Charset.forName("UTF-8")));
-                System.out.println("sign:"+sign);
+			//判断userid是否合法
+			if (userid != null && String.valueOf(userid).equals(this.userid)) {
+				//计算sign
+				StringBuilder stringBuilder = new StringBuilder();
+				stringBuilder.append(secretkey).append(userid).append(secretkey).append(ts);
+				String signSource = stringBuilder.toString();
+				System.out.println("plain:" + signSource);
+				String sign = DigestUtils.md5Hex(signSource.getBytes(Charset.forName("UTF-8")));
+				System.out.println("sign:" + sign);
 
-                //返回数据
-                response.setCharacterEncoding("UTF-8");
-                response.setContentType("application/json; charset=utf-8");
-                if (sign.equals(token)) {
-                    response.getWriter().append("{\"status\":1,\"userid\":\"" + userid + "\",\"nickname\":\"" + nickname + "\",\"avatar\":\"" + avatar + "\"}");
-                    System.out.println("{\"status\":1,\"userid\":\"" + userid + "\",\"nickname\":\"" + nickname + "\",\"avatar\":\"" + avatar + "\"}");
-                } else {
-                    response.getWriter().append("{\"status\":0,\"errorUrl\":\"\"}");
-                    System.out.println("{\"status\":0,\"errorUrl\":\"\"}");
-                }
+				//返回数据
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json; charset=utf-8");
+				if (sign.equals(token)) {
+					response.getWriter().append("{\"status\":1,\"userid\":\"" + userid + "\",\"nickname\":\"" + nickname + "\",\"avatar\":\"" + avatar + "\"}");
+					System.out.println("{\"status\":1,\"userid\":\"" + userid + "\",\"nickname\":\"" + nickname + "\",\"avatar\":\"" + avatar + "\"}");
+				} else {
+					response.getWriter().append("{\"status\":0,\"errorUrl\":\"\"}");
+					System.out.println("{\"status\":0,\"errorUrl\":\"\"}");
+				}
 
-            } else {
-                response.getWriter().append("{\"status\":0,\"errorUrl\":\"\"}");
-                System.out.println("{\\\"status\\\":0,\\\"errorUrl\\\":\\\"\\\"}");
-            }
+			} else {
+				response.getWriter().append("{\"status\":0,\"errorUrl\":\"\"}");
+				System.out.println("{\\\"status\\\":0,\\\"errorUrl\\\":\\\"\\\"}");
+			}
 
-        } catch (Exception e) {
-        }
-        return null;
-    }
+		} catch (Exception e) {
+		}
+		return null;
+	}
 }
--- demo/src/main/java/com/example/demo/util/APIParser.java
+++ demo/src/main/java/com/example/demo/util/APIParser.java
@@ -1,7 +1,14 @@
 package com.example.demo.util;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class APIParser {
 	/**
@@ -10,11 +17,13 @@ public class APIParser {
 	 * 3.与状态类ParamStatus一起交给MemberHandler,MemberHandler返回拿到的reqParam,同时初始化好ParamStatus
 	 * 4.与状态类MethodStatus一起交给MethodHandler,MethodHandler返回拿到的reqParam,同时初始化好MethodStatus
 	 * 5.根据状态进行 -计算Sign 和 计算重载的方法的参数         其中根据标记令哪些参数用默认值,哪些参数用传参(亦可批量去properties读取  ---若采用properties则需要syso提示一次会用到哪些参数)
-	 *                -根据 是 POST 还是 GET,调用setGET()/setPOST()       其中setGET是替换参数  setPost是将参数丢入entity
-	 *                -发起请求
+	 * -根据 是 POST 还是 GET,调用setGET()/setPOST()       其中setGET是替换参数  setPost是将参数丢入entity
+	 * -发起请求
 	 */
 
 	private String secretKey = "";
+	private List<String> params = new LinkedList<>();
+	private List<String> methods = new LinkedList<>();
 
 	public static void main(String[] args) {
 		try {
@@ -25,15 +34,20 @@ public class APIParser {
 	}
 
 	/**
-	* @Description:  2.反射读取所有变量,方法名
-	*/
+	 * @Description: 2.反射读取所有变量, 方法名
+	 */
 	public void Parse() throws ClassNotFoundException {
 		Class targetClass = Class.forName("com.example.demo.api.GetByIdTEST");
 		//获取自定字段
 		Field[] fields = targetClass.getDeclaredFields();
-		LinkedHashMap params = new LinkedHashMap<String,String>();
 		for (Field field : fields) {
-			System.out.println(field.getName());
+			this.params.add(field.getName());
 		}
+		Method[] methods = targetClass.getDeclaredMethods();
+		for (Method method : methods) {
+			this.methods.add(method.getName());
+		}
+
+		System.out.println(this.params);
 	}
 }
